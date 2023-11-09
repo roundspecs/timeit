5f172c7638b138b8805663113a9777b61d0cc3c3
roundspecs <2502.zarif@gmail.com>
2023-11-09 13:47:15 +0600

    Do frontend stuff

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, request, url_for, render_template
 app = Flask(__name__)
 
-@app.route("/")
+tasks = [
+  {"sl":1, "name":"Make a time tracker website", "duration": "1 hour"},
+  {"sl":2, "name":"Make a sandwich", "duration": "30 min"},
+]
+
+current_task = "academics"
+
+@app.get("/")
 def home():
-  return "Home"
+  dark_theme = request.args.get('theme', "light") == "dark"
+  print(request.args.get('theme', "light"), dark_theme)
+  return render_template("index.html", tasks=tasks, dark_theme=dark_theme, current_task=current_task)
+
+@app.get("/edit/<int:id>")
+def edit(id):
+  dark_theme = request.args.get('theme', "light") == "dark"
+  print(request.args.get('theme', "light"), dark_theme)
+  return render_template("edit.html", tasks=tasks, dark_theme=dark_theme)
