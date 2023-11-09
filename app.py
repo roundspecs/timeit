f8f3afad26a03369cddc83c90b0a9096d54f13f3
roundspecs <2502.zarif@gmail.com>
2023-11-09 13:56:37 +0600

    Make theme consistent

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 from flask import Flask, request, url_for, render_template
+
 app = Flask(__name__)
 
 tasks = [
-  {"sl":1, "name":"Make a time tracker website", "duration": "1 hour"},
-  {"sl":2, "name":"Make a sandwich", "duration": "30 min"},
+    {"sl": 1, "id":123, "name": "Make a time tracker website", "duration": "1 hour"},
+    {"sl": 2, "id":122, "name": "Make a sandwich", "duration": "30 min"},
 ]
 
 current_task = "academics"
 
+
 @app.get("/")
 def home():
-  dark_theme = request.args.get('theme', "light") == "dark"
-  print(request.args.get('theme', "light"), dark_theme)
-  return render_template("index.html", tasks=tasks, dark_theme=dark_theme, current_task=current_task)
+    return render_template(
+        "index.html",
+        tasks=tasks,
+        theme=request.args.get("theme", "light"),
+        current_task=current_task,
+    )
+
 
 @app.get("/edit/<int:id>")
 def edit(id):
-  dark_theme = request.args.get('theme', "light") == "dark"
-  print(request.args.get('theme', "light"), dark_theme)
-  return render_template("edit.html", tasks=tasks, dark_theme=dark_theme)
+    return render_template(
+        "edit.html", tasks=tasks, theme=request.args.get("theme", "light")
+    )
